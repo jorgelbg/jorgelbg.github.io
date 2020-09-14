969ec97e327cc489d0041ed00ec7d0b05c162902
Jorge Luis Betancourt <github@jorgelbg.me>
2020-09-15 00:29:07 +0200

    dashflare: Add additional screenshot

--- content/dashflare/_index.md
+++ content/dashflare/_index.md
@@ -2,6 +2,7 @@
 date = "2020-07-07T00:02:37+02:00"
 title = "Dashflare Waiting List"
 hidden = true
+type = "page"
 +++
 
 something something
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const { theme } = require("tailwindcss/stubs/defaultConfig.stub");
 module.exports = {
   plugins: [
     tailwindcss,
